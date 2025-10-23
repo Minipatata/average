099ed3ce18a1dac4f47158530ab37221648a42d7
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-10-23 19:05:12 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches and writes the 
+# height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+
+tree_height1=int(input("Enter the height of the tree"))
+tree_height2=int(input("Enter the height of the tree"))
+tree_height3=int(input("Enter the height of the tree"))
+tree_height4=int(input("Enter the height of the tree"))
+tree_height5=int(input("Enter the height of the tree"))
+
+average=tree_height1+tree_height2+tree_height3+tree_height4+tree_height5
+print(average/547
+      )
